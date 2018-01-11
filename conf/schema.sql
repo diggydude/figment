a5bf9991df7cdf08b07d85b78a9c470203ae9854
diggydude <jselkins@yahoo.com>
2018-01-11 17:46:33 -0600

    Revise schema

--- conf/schema.sql
+++ conf/schema.sql
@@ -1,19 +1,22 @@
-DROP VIEW IF EXISTS `figment_message_display`;
+DROP VIEW IF EXISTS `figment_message_display`, `figment_profile_display`;
 
-DROP TABLE IF EXISTS `figment_user`, `figment_profile`, `figment_avatar`,
-  `figment_banner`, `figment_color_scheme`, `figment_message`, `figment_upload`,
-  `figment_follow`, `figment_hashtag`, `figment_tagged`,`figment_like`, `figment_dislike`,
-  `figment_repost`, `figment_views`, `figment_clicks`, `figment_blocked`,
-  `figment_reason`, `figment_flagged`, `figment_banned`;
+DROP TABLE IF EXISTS `figment_avatar`,  `figment_banner`,   `figment_color_scheme`,
+  `figment_user`,    `figment_profile`, `figment_message`,  `figment_upload`,
+  `figment_follow`,  `figment_mention`, `figment_hashtag`,  `figment_tagged`,
+  `figment_like`,    `figment_dislike`, `figment_bookmark`, `figment_repost`,
+  `figment_views`,   `figment_clicks`,  `figment_blocked`,  `figment_reason`,
+  `figment_flagged`, `figment_banned`,  `figment_emoticon`;
 
 CREATE TABLE `figment_avatar` (
   `avatar_id` INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `filename`  VARCHAR(32) NOT NULL
+  `filename`  VARCHAR(32) NOT NULL,
+  UNIQUE KEY (`filename`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_banner` (
   `banner_id` INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `filename`  VARCHAR(32) NOT NULL
+  `filename`  VARCHAR(32) NOT NULL,
+  UNIQUE KEY (`filename`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_color_scheme` (
@@ -21,7 +24,8 @@ CREATE TABLE `figment_color_scheme` (
   `text_color`       VARCHAR(6) NOT NULL,
   `background_color` VARCHAR(6) NOT NULL,
   `highlight_color`  VARCHAR(6) NOT NULL,
-  `shadow_color`     VARCHAR(6) NOT NULL
+  `shadow_color`     VARCHAR(6) NOT NULL,
+  UNIQUE KEY (`text_color`, `background_color`, `highlight_color`, `shadow_color`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_user` (
@@ -62,30 +66,42 @@ CREATE TABLE `figment_message` (
   `posted_by`  INT(11)   NOT NULL,
   `posted_at`  TIMESTAMP NOT NULL DEFAULT NOW(),
   `raw_text`   TEXT      NOT NULL CHECK (CHAR_LENGTH(`raw_text`) < 2049),
-  `formatted`  TEXT      NOT NULL
+  `formatted`  TEXT      NOT NULL,
+  FOREIGN KEY (`posted_by`) REFERENCES `figment_user` (`user_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_upload` (
   `upload_id` INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `filename`  VARCHAR(32) NOT NULL
+  `filename`  VARCHAR(32) NOT NULL,
+  UNIQUE KEY (`filename`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_follow` (
   `follower` INT(11) NOT NULL,
   `followed` INT(11) NOT NULL,
+  UNIQUE KEY (`follower`, `followed`)
   FOREIGN KEY (`follower`) REFERENCES `figment_user` (`user_id`),
   FOREIGN KEY (`followed`) REFERENCES `figment_user` (`user_id`),
-  UNIQUE KEY (`follower`, `followed`)
+) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
+
+CREATE TABLE `figment_mention` (
+  `mentioned`    INT(11) NOT NULL,
+  `mentioned_in` INT(11) NOT NULL,
+  UNIQUE KEY (`mentioned`, `mentioned_in`),
+  FOREIGN KEY (`mentioned`)    REFERENCES `figment_user`    (`user_id`),
+  FOREIGN KEY (`mentioned_in`) REFERENCES `figment_message` (`message_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_hashtag` (
   `hashtag_id` INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `content`    VARCHAR(255) NOT NULL
+  `content`    VARCHAR(255) NOT NULL,
+  UNIQUE KEY (`content`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_tagged` (
   `message` INT(11) NOT NULL,
   `hashtag` INT(11) NOT NULL,
+  UNIQUE KEY (`message`, `tagged`),
   FOREIGN KEY (`message`) REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`hashtag`) REFERENCES `figment_hashtag` (`hashtag_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
@@ -93,6 +109,7 @@ CREATE TABLE `figment_tagged` (
 CREATE TABLE `figment_like` (
   `message`  INT(11) NOT NULL,
   `liked_by` INT(11) NOT NULL,
+  UNIQUE KEY (`message`, `liked_by`),
   FOREIGN KEY (`message`)  REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`liked_by`) REFERENCES `figment_user`    (`user_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
@@ -100,13 +117,23 @@ CREATE TABLE `figment_like` (
 CREATE TABLE `figment_dislike` (
   `message`     INT(11) NOT NULL,
   `disliked_by` INT(11) NOT NULL,
+  UNIQUE KEY (`message`, `disliked_by`),
   FOREIGN KEY (`message`)     REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`disliked_by`) REFERENCES `figment_user`    (`user_id`)
+) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
+
+CREATE TABLE `figment_bookmark` (
+  `message`       INT(11) NOT NULL,
+  `bookmarked_by` INT(11) NOT NULL,
+  UNIQUE KEY (`message`, `bookmarked_by`),
+  FOREIGN KEY (`message`)       REFERENCES `figment_message` (`message_id`),
+  FOREIGN KEY (`bookmarked_by`) REFERENCES `figment_user`    (`user_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 CREATE TABLE `figment_repost` (
   `original`    INT(11) NOT NULL,
   `reposted_in` INT(11) NOT NULL,
+  UNIQUE KEY (`original`, `reposted_in`),
   FOREIGN KEY (`original`)    REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`reposted_in`) REFERENCES `figment_message` (`message_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
@@ -114,6 +141,7 @@ CREATE TABLE `figment_repost` (
 CREATE TABLE `figment_reply` (
   `reply`    INT(11) NOT NULL,
   `reply_to` INT(11) NOT NULL,
+  UNIQUE KEY (`reply`, `reply_to`),
   FOREIGN KEY (`reply`)    REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`reply_to`) REFERENCES `figment_message` (`message_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
@@ -133,6 +161,7 @@ CREATE TABLE `figment_views` (
 CREATE TABLE `figment_blocked` (
   `blocker` INT(11) NOT NULL,
   `blocked` INT(11) NOT NULL,
+  UNIQUE KEY (`blocker`, `blocked`),
   FOREIGN KEY (`blocker`) REFERENCES `figment_user` (`user_id`),
   FOREIGN KEY (`blocked`) REFERENCES `figment_user` (`user_id`),
   UNIQUE KEY (`blocker`, `blocked`)
@@ -140,7 +169,8 @@ CREATE TABLE `figment_blocked` (
 
 CREATE TABLE `figment_reason` (
   `reason_id` INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  `text`      VARCAR(255) NOT NULL
+  `text`      VARCAR(255) NOT NULL,
+  UNIQUE KEY (`reason`),
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
 INSERT INTO `figment_reason` (`text`) VALUES ('Posting child pornography.');
@@ -154,6 +184,7 @@ CREATE TABLE `figment_flagged` (
   `flagged_by` INT(11)   NOT NULL,
   `flagged_at` TIMESTAMP NOT NULL DEFAULT NOW(),
   `reason`     INT(11)   NOT NULL,
+  UNIQUE KEY (`message`, `flagged_by`),
   FOREIGN KEY (`message`)    REFERENCES `figment_message` (`message_id`),
   FOREIGN KEY (`flagged_by`) REFERENCES `figment_user`    (`user_id`),
   FOREIGN KEY (`reason`)     REFERENCES `figment_reason`  (`reason_id`)
@@ -169,23 +200,72 @@ CREATE TABLE `figment_banned` (
   FOREIGN KEY (`reason`)    REFERENCES `figment_reason` (`reason_id`)
 ) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
 
+CREATE TABLE `figment_emoticon` (
+  `emoticon_id` INT(11)     NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  `code`        VARCHAR(32) NOT NULL,
+  `filename`    VARCHAR(32) NOT NULL,
+  UNIQUE KEY (`code`),
+  UNIQUE KEY (`filename`)
+) ENGINE XtraDB DEFAULT CHARACTER SET utf8 DEFAULT COLLATE latin1_swedish_ci;
+
 CREATE VIEW `figment_message_display` AS
-  SELECT `msg`.`posted_at`          AS `postedAt`,
-         `msg`.`formatted`          AS `message`,
-         `rpt`.`reply_to`           AS `replyingTo`,
-         `usr`.`user_id`            AS `userId`,
+  SELECT `msg`.`formatted`          AS `message`,
+         `msg`.`posted_at`          AS `postedAt`,
+         `usr`.`username`           AS `postedBy`,
          `prf`.`display_name`       AS `displayName`,
          `avt`.`filename`           AS `avatar`,
+         `rpt`.`reply_to`           AS `replyingToMessage`,
+         `rpa`.`username`           AS `replyingToAuthor`,
          count(`lik`.`liked_by`)    AS `likes`,
          count(`dsl`.`disliked_by`) AS `dislikes`,
          count(`rps`.`reposted_in`) AS `reposts`,
-         count(`rpl`.`reply`)       AS `replies`
+         count(`rpl`.`reply`)       AS `replies`,
+         `vue`.`views`              AS `views`
     FROM      `figment_message` AS `msg`
-    LEFT JOIN `figment_like`    AS `lik` ON `lik`.`message`   = `msg`.`message_id`
-    LEFT JOIN `figment_dislike` AS `dsl` ON `dsl`.`message`   = `msg`.`message_id`
-    LEFT JOIN `figment_repost`  AS `rps` ON `rps`.`original`  = `msg`.`message_id`
-    LEFT JOIN `figment_reply`   AS `rpt` ON `rpt`.`reply`     = `msg`.`message_id`
-    LEFT JOIN `figment_reply`   AS `rpl` ON `rpl`.`reply_to`  = `msg`.`message_id`
-    LEFT JOIN `figment_user`    AS `usr` ON `usr`.`user_id`   = `msg`.`posted_by`
-    LEFT JOIN `figment_profile` AS `prf` ON `prf`.`user_id`   = `usr`.`user_id`
-    LEFT JOIN `figment_avatar`  AS `avt` ON `avt`.`avatar_id` = `prf`.`avatar`;
+    LEFT JOIN `figment_user`    AS `usr` ON `usr`.`user_id`    = `msg`.`posted_by`
+    LEFT JOIN `figment_profile` AS `prf` ON `prf`.`user_id`    = `usr`.`user_id`
+    LEFT JOIN `figment_avatar`  AS `avt` ON `avt`.`avatar_id`  = `prf`.`avatar`
+    LEFT JOIN `figment_reply`   AS `rpt` ON `rpt`.`reply`      = `msg`.`message_id`
+    LEFT JOIN `figment_message` AS `rpm` ON `rpm`.`message_id` = `rpt`.`reply_to`
+    LEFT JOIN `figment_user`    AS `rpa` ON `rpa`.`user_id`    = `rpt`.`posted_by`
+    LEFT JOIN `figment_like`    AS `lik` ON `lik`.`message`    = `msg`.`message_id`
+    LEFT JOIN `figment_dislike` AS `dsl` ON `dsl`.`message`    = `msg`.`message_id`
+    LEFT JOIN `figment_repost`  AS `rps` ON `rps`.`original`   = `msg`.`message_id`
+    LEFT JOIN `figment_reply`   AS `rpl` ON `rpl`.`reply_to`   = `msg`.`message_id`
+    LEFT JOIN `figment_views`   AS `vue` ON `vue`.`message`    = `msg`.`message_id`;
+
+CREATE VIEW `figment_profile_display` AS
+  SELECT `usr`.`username`            AS `username`,
+         `usr`.`joined`              AS `joined`,
+         `prf`.`display_name         AS `displayName`,
+         `prf`.`biography`           AS `biography`,
+         `prf`.`birthday`            AS `birthday`,
+         `prf`.`gender`              AS `gender`,
+         `prf`.`location`            AS `location`,
+         `avt`.`filename`            AS `avatar`,
+         `ban`.`filename`            AS `banner`,
+         `clr`.`text_color`          AS `textColor`,
+         `clr`.`background_color`    AS `backgroundColor`,
+         `clr`.`shadow_color`        AS `shadowColor`,
+         `clr`.`highlight_color`     AS `highlightColor`,
+         `prf`.`pinned`              AS `pinnedMessage`,
+         count(`pst`.`message_id`)   AS `posts`,
+         count(`rps`.`message_id`)   AS `reposts`,
+         count(`rpl`.`message_id`)   AS `replies`,
+         count(`men`.`mentioned_in`) AS `mentions`,
+         count(`flr`.`follower`)     AS `followers`,
+         count(`flg`.`followed`)     AS `following`,
+         count(`blk`.`blocked_by`)   AS `haters`
+  FROM      `figment_user`         AS `usr`
+  LEFT JOIN `figment_profile`      AS `prf` ON `prf`.`user_id`     = `usr`.`user_id`
+  LEFT JOIN `figment_avatar`       AS `avt` ON `avt`.`avatar_id`   = `prf`.`avatar`
+  LEFT JOIN `figment_banner`       AS `ban` ON `ban`.`banner_id`   = `prf`.`banner`
+  LEFT JOIN `figment_color_scheme` AS `clr` ON `clr`.`scheme_id`   = `prf`.`color_scheme`
+  LEFT JOIN `figment_message`      AS `pst` ON `pst`.`posted_by`   = `usr`.`user_id`
+  LEFT JOIN `figment_repost`       AS `rps` ON `rps`.`reposted_in` = `pst`.`message`
+  LEFT JOIN `figment_reply`        AS `rpl` ON `rpl`.`reply`       = `pst`.`message`
+  LEFT JOIN `figment_mention`      AS `men` ON `men`.`mentioned`   = `usr`.`user_id`
+  LEFT JOIN `figment_follow`       AS `flr` ON `flr`.`followed`    = `usr`.`user_id`
+  LEFT JOIN `figment_follow`       AS `flg` ON `flg`.`follower`    = `usr`.`user_id`
+  LEFT JOIN `figment_blocked`      AS `blk` ON `blk`.`blocked`     = `usr`.`user_id`;
+
