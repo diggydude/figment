4bb47ca8bcba3ceaedc8f1fe0336ddf655796c99
diggydude <jselkins@yahoo.com>
2018-01-27 12:51:50 -0600

    Update index.php

--- index.php
+++ index.php
@@ -2,22 +2,19 @@
 
   require_once(__DIR__ . '/conf/config.php');
 
-  $session  = Session::instance();
-  $route    = getRoute($_SERVER['REQUEST_URI']);
-  $pdo      = Db::connect('figment', config('database'));
-  $response = HtmlResponse::instance();
-
-  if (!$route) {
-    $response->error(404);
-    exit(1);
-  }
-
-  $baseUri       = config('baseUri');
-  $response->js  = $baseUri . "/client/js/jquery.js";
-  $response->js  = $baseUri . "/client/js/figment.js";
-  $response->css = $baseUri . "/client/css/figment.css"; 
-
   try {
+    $session  = Session::instance();
+    $route    = getRoute($_SERVER['REQUEST_URI']);
+    $pdo      = Db::connect('figment', config('database'));
+    $response = HtmlResponse::instance();
+    if (!$route) {
+      $response->error(404);
+      exit(1);
+    }
+    $baseUri       = config('baseUri');
+    $response->js  = $baseUri . "/client/js/jquery.js";
+    $response->js  = $baseUri . "/client/js/figment.js";
+    $response->css = $baseUri . "/client/css/figment.css"; 
     $controller = Controller::create($route->controller);
     $method     = (property_exists($controller, $method)) ? $route->method : "index";
     call_user_func(array($controller, $method));
