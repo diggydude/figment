856e2ec315295bc5dc5568f68c00f5a7005b40e5
diggydude <jselkins@yahoo.com>
2018-01-29 10:00:34 -0600

    Refactor Controller base class

--- lib/classes/Controller.php
+++ lib/classes/Controller.php
@@ -3,19 +3,50 @@
   class Controller
   {
 
+    protected
+
+      $_method,
+      $_response,
+      $_template,
+      $_currUser;
+
     public static function create($type, $params = null)
     {
       $file = config('ctrlDir') . "/" . $type . ".php";
       if (!file_exists($file)) {
-        throw new Exception(__METHOD__ . ' > Unknown controller: ' . $type);
+        throw new Exception(__METHOD__ . ' > Unknown controller type: ' . $type);
       }
       require_once($file);
-      $controller = new $type();
-      foreach (get_object_vars($params) as $k => $v) {
-        $controller->$k = $v;
-      }
+      $className  = $type . "Controller";
+      $controller = new $className($params);
       return $controller;
     } // create
+
+    protected function __construct($params)
+    {
+      $baseUri         = config('baseUri');
+      $this->_method   = (property_exists($params, 'method'))   ? $params->method   : "index";
+      $this->_template = (property_exists($params, 'template')) ? $params->template : "";
+      $this->_currUser = new User(Session::instance()->username);
+      switch ($params->responseType) {
+        case "html":
+        default:
+          $this->_response      = new HtmlResponse();
+          $this->_response->js  = $baseUri . "/client/js/jquery.js";
+          $this->_response->js  = $baseUri . "/client/js/figment.js";
+          $this->_response->css = $baseUri . "/client/css/figment.css";
+          break;
+        case "json":
+          $this->_response = new JsonResponse();
+          break;
+      }
+    } // __construct
+
+    public function run()
+    {
+      call_user_func(array($this, $this->_method));
+      $this->_response->send();
+    } // run
 
   } // Controller
 
