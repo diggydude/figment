9ecbb90ba6787dfc288da255e872f4d230db1964
diggydude <jselkins@yahoo.com>
2018-01-29 10:05:57 -0600

    Rename config args for clarity

--- conf/config.php
+++ conf/config.php
@@ -2,6 +2,7 @@
 
   error_reporting(E_ALL ^ E_NOTICE);
   set_time_limit(120);
+  date_default_timezone_set(config('timezone'));
 
   require_once(realpath(__DIR__ . '/../lib/functions/config.php'));
 
@@ -44,64 +45,64 @@
   );
   config('routes',     array(
                          (object) array(
-                           'pattern'    => '/^(message)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'method'),
-                           'response'   => 'json'
+                           'pattern'      => '/^(message)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'method'),
+                           'responseTyep' => 'json'
                          ),
                          (object) array(
-                           'pattern'    => '/^(feed)$/',
-                           'components' => array('controller'),
-                           'response'   => 'html',
-                           'templates'  => (object) array(
-                                             'index' => 'feed/index.php'
-                                           )
+                           'pattern'      => '/^(feed)$/',
+                           'components'   => array('controller'),
+                           'responseType' => 'html',
+                           'templates'    => (object) array(
+                                               'index' => 'feed/index.php'
+                                             )
                          ),
                          (object) array(
-                           'pattern'    => '/^(profile)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'username', 'method'),
-                           'response'   => 'html',
-                           'templates'  => (object) array(
-                                             'index'      => 'profile/index.php',
-                                             'navigator'  => 'profile/navigator.php',
-                                             'edit'       => 'profile/edit.php',
-                                             'editAvatar' => 'profile/editAvatar.php',
-                                             'editBanner' => 'profile/editBanner.php',
-                                             'editColors' => 'profile/editColors.php'
-                                           )
+                           'pattern'      => '/^(profile)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'username', 'method'),
+                           'responseType' => 'html',
+                           'templates'    => (object) array(
+                                               'index'      => 'profile/index.php',
+                                               'navigator'  => 'profile/navigator.php',
+                                               'edit'       => 'profile/edit.php',
+                                               'editAvatar' => 'profile/editAvatar.php',
+                                               'editBanner' => 'profile/editBanner.php',
+                                               'editColors' => 'profile/editColors.php'
+                                             )
                          ),
                          (object) array(
-                           'pattern'    => '/^(profile)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'username'),
-                           'response'   => 'html',
-                           'templates'  => (object) array(
-                                             'index' => 'profile/index.php'
-                                           )
+                           'pattern'      => '/^(profile)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'username'),
+                           'responseType' => 'html',
+                           'templates'    => (object) array(
+                                               'index' => 'profile/index.php'
+                                             )
                          ),
                          (object) array(
-                           'pattern'    => '/^(hashtag)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'hashtag'),
-                           'response'   => 'html',
-                           'templates'  => (object) array(
-                                             'index' => 'hashtag/index.php'
-                                           )
+                           'pattern'      => '/^(hashtag)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'hashtag'),
+                           'responseType' => 'html',
+                           'templates'    => (object) array(
+                                               'index' => 'hashtag/index.php'
+                                             )
                          ),
                          (object) array(
-                           'pattern'    => '/^(api)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'module', 'uid', 'method'),
-                           'response'   => 'json'
+                           'pattern'      => '/^(api)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'module', 'uid', 'method'),
+                           'responseType' => 'json'
                          ),
                          (object) array(
-                           'pattern'    => '/^(api)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'module', 'method'),
-                           'response'   => 'json'
+                           'pattern'      => '/^(api)\/([a-zA-Z0-9]*)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'module', 'method'),
+                           'responseType' => 'json'
                          ),
                          (object) array(
-                           'pattern'    => '/^(user)\/([a-zA-Z0-9]*)$/',
-                           'components' => array('controller', 'method'),
-                           'response'   => 'html',
-                           'templates'  => (object) array(
-                                             'login' => 'user/login.php'
-                                           )
+                           'pattern'      => '/^(user)\/([a-zA-Z0-9]*)$/',
+                           'components'   => array('controller', 'method'),
+                           'responseType' => 'html',
+                           'templates'    => (object) array(
+                                               'login' => 'user/login.php'
+                                             )
                          )
                        )
   );
@@ -114,8 +115,6 @@
   config('tplDir',     config('baseDir') . '/templates/');
   config('uploadDir',  config('baseDir') . '/client/images/uploads/');
   config('movedList',  config('dataDir') . 'moved.json');
-
-  date_default_timezone_set(config('timezone'));
 
   require_once(config('libDir') . 'functions/getRequestHeaders.php');
   require_once(config('libDir') . 'classes/Db.php');
