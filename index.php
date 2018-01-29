ecdf0b9aa1d52fcd78c83cf25130b89eb6fc4d2b
diggydude <jselkins@yahoo.com>
2018-01-29 10:14:27 -0600

    Mv controller setup from index.php to Controller class

--- index.php
+++ index.php
@@ -3,42 +3,23 @@
   require_once(__DIR__ . '/conf/config.php');
 
   try {
-    $session       = Session::instance();
-    $route         = getRoute($_SERVER['REQUEST_URI']);
-    $pdo           = Db::connect('figment', config('database'));
-    $response      = HtmlResponse::instance();
-    $baseUri       = config('baseUri');
-    $response->js  = $baseUri . "/client/js/jquery.js";
-    $response->js  = $baseUri . "/client/js/figment.js";
-    $response->css = $baseUri . "/client/css/figment.css"; 
+    $session = Session::instance();
+    $pdo     = Db::connect('figment', config('database'));
+    $route   = getRoute($_SERVER['REQUEST_URI']);
     if (($route === null) || !property_exists($route, 'controller')) {
       if ($session->username == "") {
         $response->redirect(config('baseUri') . '/user/login');
         exit(0);
       }
-      $controller = Controller::create('feed');
-      call_user_func(array($controller, 'index'));
-      $response->send();
-      exit(0);
-    }
-    if (property_exists($route, 'method')) {
-      $method = $route->method;
-      unset($route->method);
+      $params     = (object) array(
+                      'emthod' => 'index',
+                    );
+      $controller = Controller::create('feed', $params);
     }
     else {
-      $method = null;
+      $controller = Controller::create($route->controller, $route);
     }
-    $controller = $route->controller;
-    unset($route->controller);
-    $params     = $route;
-    $controller = Controller::create($controller, $params);
-    if (($method !== null) && method_exists($controller, $method)) {
-      call_user_func(array($controller, $method));
-    }
-    else {
-      call_user_func(array($controller, 'index'));
-    }
-    $response->send();
+    $controller->run();
     exit(0);
   }
   catch (Exception $e) {
